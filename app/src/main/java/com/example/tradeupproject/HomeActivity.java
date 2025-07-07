80d3c558b6747114e461d052a5530bc9d86047cd
whiskeeyy <kietduongdinh@gmail.com>
2025-07-08 02:25:19 +0700

    HomeActivity updated

--- app/src/main/java/com/example/tradeupproject/HomeActivity.java
+++ app/src/main/java/com/example/tradeupproject/HomeActivity.java
@@ -1,4 +1,50 @@
 package com.example.tradeupproject;
 
-public class HomeActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        bottomNavigation = findViewById(R.id.bottomNavigation);
+
+        bottomNavigation.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.nav_home) {
+                // Already in Home
+                return true;
+
+            } else if (itemId == R.id.nav_listings) {
+                // TODO: Go to ListingsActivity
+                return true;
+
+            } else if (itemId == R.id.nav_post) {
+                // TODO: Go to PostActivity
+                return true;
+
+            } else if (itemId == R.id.nav_alerts) {
+                // TODO: Go to AlertsActivity
+                return true;
+
+            } else if (itemId == R.id.nav_profile) {
+                startActivity(new Intent(this, ProfileActivity.class));
+                return true;
+            }
+
+            return false;
+        });
+    }
 }
